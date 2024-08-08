4e096ece407529f71f273893c8fa45b0ca51ba6b
Nicola Ferralis <feranick@hotmail.com>
2024-08-08 18:01:54 +0200

    Added support for Ubuntu:24.04
    24.04 introduces a new way to handle repositories, using sources rather than lists.

--- docker/update_sources.orig.sh
+++ docker/update_sources.orig.sh
@@ -19,44 +19,8 @@
 
 sed -i "s/deb\ /deb \[arch=amd64\]\ /g" /etc/apt/sources.list
 
-if [ ${UBUNTU_CODENAME} == "noble" ]; then
-
-echo "NOBLE"
-
-rm /etc/apt/sources.list.d/ubuntu.sources
-cat <<EOT >> /etc/apt/sources.list.d/ubuntu.sources
-
-Types: deb
-URIs: http://archive.ubuntu.com/ubuntu/
-Suites: ${UBUNTU_CODENAME} ${UBUNTU_CODENAME}-updates ${UBUNTU_CODENAME}-backports
-Components: main universe restricted multiverse
-Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
-Architectures: amd64
-
-## Ubuntu security updates. Aside from URIs and Suites,
-## this should mirror your choices in the previous section.
-Types: deb
-URIs: http://security.ubuntu.com/ubuntu/
-Suites: ${UBUNTU_CODENAME}-security
-Components: main universe restricted multiverse
-Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
-Architectures: amd64
-
-Types: deb
-URIs: http://ports.ubuntu.com/ubuntu-ports
-Suites: ${UBUNTU_CODENAME} ${UBUNTU_CODENAME}-updates ${UBUNTU_CODENAME}-security
-Components: main universe
-Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
-Architectures: arm64 armhf
-
-EOT
-
-else
-
 cat <<EOT >> /etc/apt/sources.list
 deb [arch=arm64,armhf] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_CODENAME} main universe
 deb [arch=arm64,armhf] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_CODENAME}-updates main universe
 deb [arch=arm64,armhf] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_CODENAME}-security main universe
 EOT
-
-fi
